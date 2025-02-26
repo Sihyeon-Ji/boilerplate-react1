e1e90150b5bd574fa7f6039fcd4fde29589dcb8d
shji <shji@dtol.co.kr>
2025-02-26 17:05:17 +0900

    🐛 [버그] 문법 수정

--- projects/common/src/components/ui/example/demos/sonner-demo.tsx
+++ projects/common/src/components/ui/example/demos/sonner-demo.tsx
@@ -11,7 +11,9 @@ export function SonnerDemo() {
 					description: "Sunday, December 03, 2023 at 9:00 AM",
 					action: {
 						label: "Undo",
-						onClick: () => console.log("Undo"),
+						onClick: () => {
+							console.log("Undo");
+						},
 					},
 				})
 			}
